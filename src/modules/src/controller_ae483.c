21db3bfaf662024c25792b0ed10fa55700d50c85
Nagesh Eranki <nageswararao.160324@gmail.com>
2023-12-15 14:26:57 -0600

    src

---
+++ src/modules/src/controller_ae483.c
@@ -0,0 +1,160 @@
+#include "controller_ae483.h"
+#include "stabilizer_types.h"
+#include "power_distribution.h"
+#include "log.h"
+#include "param.h"
+#include "num.h"
+#include "math3d.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+// Sensor measurements
+// - tof (from the z ranger on the flow deck)
+static uint16_t tof_count = 0;
+static float tof_distance = 0.0f;
+// - flow (from the optical flow sensor on the flow deck)
+static uint16_t flow_count = 0;
+static float flow_dpixelx = 0.0f;
+static float flow_dpixely = 0.0f;
+
+// An example parameter
+static bool use_observer = false;
+
+// Motor power commands -> EXPERIMENTAL
+static uint16_t i1 = 0;
+static uint16_t i2 = 0;
+static uint16_t i3 = 0;
+static uint16_t i4 = 0;
+
+// Timestamp of first packet to CF
+static uint32_t start = 0;
+static bool started = 0;
+
+void ae483UpdateWithTOF(tofMeasurement_t *tof)
+{
+  tof_distance = tof->distance;
+  tof_count++;
+}
+
+void ae483UpdateWithFlow(flowMeasurement_t *flow)
+{
+  flow_dpixelx = flow->dpixelx;
+  flow_dpixely = flow->dpixely;
+  flow_count++;
+}
+
+void ae483UpdateWithDistance(distanceMeasurement_t *meas)
+{
+  // If you have a loco positioning deck, this function will be called
+  // each time a distance measurement is available. You will have to write
+  // code to handle these measurements. These data are available:
+  //
+  //  meas->anchorId  uint8_t   id of anchor with respect to which distance was measured
+  //  meas->x         float     x position of this anchor
+  //  meas->y         float     y position of this anchor
+  //  meas->z         float     z position of this anchor
+  //  meas->distance  float     the measured distance
+}
+
+void ae483UpdateWithPosition(positionMeasurement_t *meas)
+{
+  // This function will be called each time you send an external position
+  // measurement (x, y, z) from the client, e.g., from a motion capture system.
+  // You will have to write code to handle these measurements. These data are
+  // available:
+  //
+  //  meas->x         float     x component of external position measurement
+  //  meas->y         float     y component of external position measurement
+  //  meas->z         float     z component of external position measurement
+}
+
+void ae483UpdateWithPose(poseMeasurement_t *meas)
+{
+  // This function will be called each time you send an external "pose" measurement
+  // (position as x, y, z and orientation as quaternion) from the client, e.g., from
+  // a motion capture system. You will have to write code to handle these measurements.
+  // These data are available:
+  //
+  //  meas->x         float     x component of external position measurement
+  //  meas->y         float     y component of external position measurement
+  //  meas->z         float     z component of external position measurement
+  //  meas->quat.x    float     x component of quaternion from external orientation measurement
+  //  meas->quat.y    float     y component of quaternion from external orientation measurement
+  //  meas->quat.z    float     z component of quaternion from external orientation measurement
+  //  meas->quat.w    float     w component of quaternion from external orientation measurement
+}
+
+void ae483UpdateWithData(const struct AE483Data* data)
+{
+  // This function will be called each time AE483-specific data are sent
+  // from the client to the drone. You will have to write code to handle
+  // these data. For the example AE483Data struct, these data are:
+  //
+  //  data->x         float
+  //  data->y         float
+  //  data->z         float
+  //
+  // Exactly what "x", "y", and "z" mean in this context is up to you.
+  if(!started)
+  {
+    start = xTaskGetTickCount();
+    started = 1;
+  }
+
+  i1 = data->m1;
+  i2 = data->m2;
+  i3 = data->m3;
+  i4 = data->m4;
+}
+
+
+void controllerAE483Init(void)
+{
+  // Do nothing
+}
+
+bool controllerAE483Test(void)
+{
+  // Do nothing (test is always passed)
+  return true;
+}
+
+void controllerAE483(control_t *control,
+                     setpoint_t *setpoint,
+                     const sensorData_t *sensors,
+                     const state_t *state,
+                     const uint32_t tick)
+{
+  if (RATE_DO_EXECUTE(ATTITUDE_RATE, tick)) {
+    // Whatever is in here executes at 500 Hz
+    // powerSet(m1, m2, m3, m4);
+    powerSet(10000, 0, 0, 0);
+
+  }
+
+  // if (RATE_DO_EXECUTE(POSITION_RATE, tick)) {
+  //   // Whatever is in here executes at 100 Hz
+
+  // }
+
+  // // By default, we do nothing (set all motor power commands to zero)
+  // powerSet(0, 0, 0, 0);
+}
+
+//              1234567890123456789012345678 <-- max total length
+//              group   .name
+LOG_GROUP_START(ae483log)
+LOG_ADD(LOG_UINT16,         num_tof,                &tof_count)
+LOG_ADD(LOG_UINT16,         num_flow,               &flow_count)
+LOG_ADD(LOG_UINT16,     i1,                      &i1)
+LOG_ADD(LOG_UINT16,     i2,                      &i2)
+LOG_ADD(LOG_UINT16,     i3,                      &i3)
+LOG_ADD(LOG_UINT16,     i4,                      &i4)
+LOG_ADD(LOG_UINT32,     start,                      &start)
+LOG_GROUP_STOP(ae483log)
+
+//                1234567890123456789012345678 <-- max total length
+//                group   .name
+PARAM_GROUP_START(ae483par)
+PARAM_ADD(PARAM_UINT8,     use_observer,            &use_observer)
+PARAM_GROUP_STOP(ae483par)
